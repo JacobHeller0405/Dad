cd844c8abdab9bfee7a9908457b2245d69090b6a
JacobHeller0405 <92735678+JacobHeller0405@users.noreply.github.com>
2022-03-06 20:09:38 -0500

    e
    e

--- dad.js
+++ dad.js
@@ -6,9 +6,22 @@ var answerList = [
     'depressed',
     'not feeling well',
 	];
+
 var randomNumber = Math.floor(Math.random() * answerList.length);
+
 var name = answerList[randomNumber];
-punchLine.onclick = function() {
-  document.getElementById('joke').textContent = 'Hi ' + name + ' Im dad!';
-}
+
 document.getElementById('name').textContent = name;
+
+reRoll.onclick = function() {
+	var randomNumber = Math.floor(Math.random() * answerList.length);
+	var name = answerList[randomNumber];
+	document.getElementById('name').textContent = name;
+	if (document.getElementById('joke').textContent !== '') {
+		document.getElementById('joke').textContent = "Hi " + name + ' Im dad!';
+	}
+
+	punchLine.onclick = function() {
+	  document.getElementById('joke').textContent = 'Hi ' + name + ' Im dad!';
+	}
+}
